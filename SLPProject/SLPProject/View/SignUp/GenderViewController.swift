53b6455b5767dac09c52263c4bdb2c4da7ce0d17
MosonLee <ron222@naver.com>
2022-11-11 05:38:32 +0900

    feat: gender VC + VM

--- SLPProject/SLPProject/View/SignUp/GenderViewController.swift
+++ SLPProject/SLPProject/View/SignUp/GenderViewController.swift
@@ -22,25 +22,31 @@ final class GenderViewController: UIViewController {
     private let nextButton = UIButton()
     
     private let viewModel = GenderViewModel()
-    private let viewDidLoadEvent = PublishRelay<Void>()
     private let disposeBag = DisposeBag()
     
     private lazy var input = GenderViewModel.Input(
-        
+        backButtonTapped: backButton.rx.tap.asSignal(),
+        boyButtonTapped: boyButton.rx.tap.asSignal(),
+        girlButtonTapped: girlButton.rx.tap.asSignal(),
+        nextButtonTapped: nextButton.rx.tap.asSignal()
     )
     
-    //    private lazy var output = viewModel.transform(input: input)
+    private lazy var output = viewModel.transform(input: input)
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setComponents()
         setConstraints()
         bind()
-        viewDidLoadEvent.accept(())
     }
     
     private func setComponents() {
-        [textLabel, subTextLabel, boyButton,girlButton, nextButton].forEach {
+        [textLabel, subTextLabel, boyButton, girlButton, nextButton].forEach {
             view.addSubview($0)
         }
         setComponentsValue()
@@ -59,9 +65,20 @@ final class GenderViewController: UIViewController {
             make.leading.equalTo(52)
             make.trailing.equalTo(-51)
             make.height.equalTo(26)
-            //            make.bottom.equalTo(emailTextField.snp.top).inset(-76)
+            make.bottom.equalTo(boyButton.snp.top).inset(-32)
         }
-        
+        boyButton.snp.makeConstraints { make in
+            make.leading.equalTo(15)
+            make.height.equalTo(120)
+            make.width.equalTo(166)
+            make.bottom.equalTo(nextButton.snp.top).inset(-32)
+        }
+        girlButton.snp.makeConstraints { make in
+            make.height.equalTo(120)
+            make.trailing.equalToSuperview().inset(15)
+            make.bottom.equalTo(nextButton.snp.top).inset(-32)
+            make.width.equalTo(166)
+        }
         nextButton.snp.makeConstraints { make in
             make.leading.equalTo(16)
             make.trailing.equalTo(-16)
@@ -73,22 +90,35 @@ final class GenderViewController: UIViewController {
         view.backgroundColor = SLPAssets.CustomColor.white.color
         textLabel.numberOfLines = 1
         textLabel.textAlignment = .center
-        textLabel.text = SLPAssets.RawString.enterEmail.text
+        textLabel.text = SLPAssets.RawString.selectGender.text
         textLabel.font = UIFont.boldSystemFont(ofSize: 20)
         
-        subTextLabel.text = SLPAssets.RawString.emailSubtext.text
+        subTextLabel.text = SLPAssets.RawString.genderRequired.text
         subTextLabel.font = UIFont.boldSystemFont(ofSize: 16)
         subTextLabel.textColor = SLPAssets.CustomColor.gray7.color
         subTextLabel.textAlignment = .center
         nextButton.layer.cornerRadius = 8
         nextButton.setTitle(SLPAssets.RawString.next.text, for: .normal)
         nextButton.backgroundColor = SLPAssets.CustomColor.gray6.color
+        
+        boyButton.setImage(SLPAssets.CustomImage.boyButton.image, for: .normal)
+        girlButton.setImage(SLPAssets.CustomImage.girlButton.image, for: .normal)
     }
     
     private func setNavigation() {
         backButton = UIBarButtonItem(image: SLPAssets.CustomImage.backButton.image, style: .plain, target: navigationController, action: nil)
         backButton.tintColor = SLPAssets.CustomColor.black.color
         navigationItem.leftBarButtonItem = backButton
+    }
+    
+    private func setBoyButtonHighlightened() {
+        boyButton.backgroundColor = SLPAssets.CustomColor.whiteGreen.color
+        girlButton.backgroundColor = SLPAssets.CustomColor.gray3.color
+    }
+    
+    private func setGirlButtonHighlightened() {
+        girlButton.backgroundColor = SLPAssets.CustomColor.whiteGreen.color
+        boyButton.backgroundColor = SLPAssets.CustomColor.gray3.color
     }
     
     private func setNextButtonAble() {
@@ -98,5 +128,39 @@ final class GenderViewController: UIViewController {
     private func setNextButtonDisabled() {
         nextButton.backgroundColor = SLPAssets.CustomColor.gray6.color
     }
-    private func bind() { }
+    
+    private func bind() {
+        output.popVC
+            .emit(onNext: { [weak self] _ in
+                self?.navigationController?.popViewController(animated: true)
+            })
+            .disposed(by: disposeBag)
+        
+        output.showMainVC
+            .emit(onNext: { [weak self] _ in
+                let vc = MainViewController()
+                self?.navigationController?.pushViewController(vc, animated: true)
+            })
+            .disposed(by: disposeBag)
+        
+        output.highlightBoyButton
+            .emit(onNext: { [weak self] _ in
+                self?.setBoyButtonHighlightened()
+                self?.setNextButtonAble()
+            })
+            .disposed(by: disposeBag)
+        
+        output.highlightGirlButton
+            .emit(onNext: { [weak self] _ in
+                self?.setGirlButtonHighlightened()
+                self?.setNextButtonAble()
+            })
+            .disposed(by: disposeBag)
+        
+        output.showToast
+            .emit(onNext: { [weak self] text in
+                self?.view.makeToast(text)
+            })
+            .disposed(by: disposeBag)
+    }
 }
--- SLPProject/SLPProject/ViewModel/SignUp/GenderViewModel.swift
+++ SLPProject/SLPProject/ViewModel/SignUp/GenderViewModel.swift
@@ -13,14 +13,77 @@ import RxSwift
 final class GenderViewModel {
     
     struct Input {
-        
+        let backButtonTapped: Signal<Void>
+        let boyButtonTapped: Signal<Void>
+        let girlButtonTapped: Signal<Void>
+        let nextButtonTapped: Signal<Void>
     }
     
     struct Output {
-        
+        let highlightBoyButton: Signal<Void>
+        let highlightGirlButton: Signal<Void>
+        let ableNextButton: Signal<Void>
+        let popVC: Signal<Void>
+        let showToast: Signal<String>
+        let showMainVC: Signal<Void>
     }
     
-//    func transform(input: Input) -> Output {
-//        
-//    }
+    private let highlightBoyButtonRelay = PublishRelay<Void>()
+    private let highlightGirlButtonRelay = PublishRelay<Void>()
+    private let ableNextButtonRelay = PublishRelay<Void>()
+    private let popVCRelay = PublishRelay<Void>()
+    private let showToastRelay = PublishRelay<String>()
+    private let showMainVCRelay = PublishRelay<Void>()
+    
+    private let disposeBag = DisposeBag()
+    
+    func transform(input: Input) -> Output {
+        input.backButtonTapped
+            .emit(onNext: { [weak self] _ in
+                self?.popVCRelay.accept(())
+                UserDefaults.gender = -1
+            })
+            .disposed(by: disposeBag)
+        
+        input.boyButtonTapped
+            .emit(onNext: { [weak self] _ in
+                self?.highlightBoyButtonRelay.accept(())
+                UserDefaults.gender = 1
+                self?.ableNextButtonRelay.accept(())
+            })
+            .disposed(by: disposeBag)
+        
+        input.girlButtonTapped
+            .emit(onNext: { [weak self] _ in
+                self?.highlightGirlButtonRelay.accept(())
+                UserDefaults.gender = 0
+                self?.ableNextButtonRelay.accept(())
+            })
+            .disposed(by: disposeBag)
+        
+        input.nextButtonTapped
+            .emit(onNext: { [weak self] _ in
+                
+                if UserDefaults.gender != -1 {
+                    self?.showMainVCRelay.accept(())
+                } else {
+                    self?.showToastRelay.accept(SLPAssets.RawString.selectGender.text)
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        return Output(
+            highlightBoyButton: highlightBoyButtonRelay.asSignal(),
+            highlightGirlButton: highlightGirlButtonRelay.asSignal(),
+            ableNextButton: ableNextButtonRelay.asSignal(),
+            popVC: popVCRelay.asSignal(),
+            showToast: showToastRelay.asSignal(),
+            showMainVC: showMainVCRelay.asSignal()
+        )
+    }
+}
+
+//MARK: 서버 통신 로직 구현 예정
+extension GenderViewModel {
+    
 }
