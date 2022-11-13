6a1998388e90c8fb11c01c2bd2c90dbb252c449d
MosonLee <ron222@naver.com>
2022-11-13 19:08:51 +0900

    add: LauncScreen

---
+++ SLPProject/SLPProject/View/Initial/InitialViewController.swift
@@ -0,0 +1,33 @@
+//
+//  InitialViewController.swift
+//  SLPProject
+//
+//  Created by 이승후 on 2022/11/13.
+//
+
+import UIKit
+
+final class InitialViewController: UIViewController {
+    
+    private let splashImage = UIImageView()
+    private let splashTextImage = UIImageView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setComponents()
+        setConstraints()
+        bind()
+    }
+    
+    private func setComponents() {
+        
+    }
+    
+    private func setConstraints() {
+        
+    }
+    
+    private func bind() {
+        
+    }
+}
---
+++ SLPProject/SLPProject/ViewModel/Initial/InitialViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  InitialViewModel.swift
+//  SLPProject
+//
+//  Created by 이승후 on 2022/11/13.
+//
+
+import Foundation
