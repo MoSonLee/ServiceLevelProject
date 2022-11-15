4944731f01992a9fa912282ec6e8dfa52469e9e3
MosonLee <ron222@naver.com>
2022-11-16 01:14:26 +0900

    feat: MySecondInfo Initial layout

--- SLPProject/SLPProject/ViewCell/TableViewCell/MySecondInfoTableViewCell.swift
+++ SLPProject/SLPProject/ViewCell/TableViewCell/MySecondInfoTableViewCell.swift
@@ -42,30 +42,80 @@ class MySecondInfoTableViewCell: UITableViewCell {
     }
     
     func setFirstCell() {
-        
+        profileNameLabel.snp.makeConstraints { make in
+            make.top.bottom.equalTo(26)
+            make.left.equalToSuperview().offset(16)
+            make.right.equalTo(showInfoButton.snp.left).offset(-10)
+        }
+        showInfoButton.snp.makeConstraints { make in
+            make.top.bottom.equalTo(26)
+            make.right.equalToSuperview().offset(-26)
+            make.width.equalTo(6)
+            make.height.equalTo(12)
+        }
+    }
+    
+    func setLeftLabel() {
+        leftLabel.snp.makeConstraints { make in
+            make.left.equalToSuperview()
+            make.width.equalTo(97)
+            make.centerY.equalToSuperview()
+        }
     }
     
     func setSecondCell() {
-        
+       setLeftLabel()
+        boyButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.right.equalTo(girlButton.snp.left).offset(-8)
+            make.width.equalTo(56)
+            make.height.equalTo(48)
+        }
+        girlButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.right.equalToSuperview()
+            make.width.equalTo(56)
+            make.height.equalTo(48)
+        }
     }
     
     func setThirdCell() {
-        
+        setLeftLabel()
+        textField.snp.makeConstraints { make in
+            make.height.equalToSuperview()
+            make.right.equalToSuperview()
+            make.width.equalTo(164)
+        }
     }
     
     func setFourthCell() {
-        
+        setLeftLabel()
+        switchButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.width.equalTo(52)
+            make.height.equalTo(28)
+        }
     }
     
     func setFifthCell() {
-        
+        setLeftLabel()
+        ageLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.right.equalToSuperview()
+            make.width.equalTo(44)
+            make.height.equalTo(22)
+        }
     }
     
     func setSixCell() {
-        
+        doubleSlider.snp.makeConstraints { make in
+            make.left.right.equalToSuperview()
+            make.centerY.equalToSuperview()
+            make.height.equalTo(4)
+        }
     }
     
     func setSeventhCell() {
-        
+        setLeftLabel()
     }
 }
