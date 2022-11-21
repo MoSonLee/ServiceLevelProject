b9b8b911a5f9a43fbfe7d2610c9d9d5fd6467e39
MosonLee <ron222@naver.com>
2022-11-21 11:55:34 +0900

    fix: Result model

--- SLPProject/SLPProject/Model/SeSACSearchResultModel.swift
+++ SLPProject/SLPProject/Model/SeSACSearchResultModel.swift
@@ -12,48 +12,17 @@ struct SeSACSearchResultModel: Codable {
         let dictionary: [String: Any] = [
             "fromQueueDB": fromQueueDB,
             "fromQueueDBRequested": fromQueueDBRequested,
-            "studylist": studylist,
-            "reviews" : reviews,
-            "reputation" : reputation,
-            "uid" : uid,
-            "nick" : nick,
-            "gender" : gender,
-            "type" : type,
-            "sesac" : sesac,
-            "type" : type,
-            "background" : background,
-            "lat" : lat,
-            "long" : long,
             "fromRecommend": fromRecommend
         ]
         return dictionary
     }
     
     let fromQueueDB: [QueueDB]
-    let fromQueueDBRequested: [QueueDBRequested]
-    let studylist: [String]
-    let reviews: [String]
-    let reputation: [String]
-    let uid: String
-    let nick: String
-    let gender: Int
-    let type: Int
-    let sesac: Int
-    let background: Int
-    let lat: Double
-    let long: Double
+    let fromQueueDBRequested: [QueueDB]
     let fromRecommend: [String]
 }
 
 struct QueueDB: Codable {
-    let uid, nick: String
-    let lat, long: Double
-    let reputation: [Int]
-    let studylist, reviews: [String]
-    let gender, type, sesac, background: Int
-}
-
-struct QueueDBRequested: Codable {
     let uid, nick: String
     let lat, long: Double
     let reputation: [Int]
