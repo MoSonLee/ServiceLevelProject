b6109d25ebbd37d6c101ca70e367a6da7597f10d
MosonLee <ron222@naver.com>
2022-11-23 01:32:45 +0900

    add: SearchCollection Rxdatasource Model

---
+++ SLPProject/SLPProject/Model/SearchCollecionModel.swift
@@ -0,0 +1,41 @@
+//
+//  SearchCollecionModel.swift
+//  SLPProject
+//
+//  Created by 이승후 on 2022/11/23.
+//
+
+
+import Foundation
+
+import RxDataSources
+
+struct SearchCollecionModel {
+    var title: String
+    var buttonImageString: String
+}
+
+struct SearchCollecionSectionModel {
+    var header: String
+    var items: [SearchCollecionModel]
+}
+
+extension SearchCollecionModel: IdentifiableType, Equatable {
+    typealias Identity = String
+    var identity: String {
+        return UUID().uuidString
+    }
+}
+
+extension SearchCollecionSectionModel: AnimatableSectionModelType {
+    typealias Item = SearchCollecionModel
+    typealias Identity = String
+    var identity: String {
+        return header
+    }
+    
+    init(original: SearchCollecionSectionModel, items: [Item]) {
+        self = original
+        self.items = items
+    }
+}
