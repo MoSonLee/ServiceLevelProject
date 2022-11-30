b4ca30c06322fc723a50ffbc53bada1cc710e60d
MosonLee <ron222@naver.com>
2022-12-01 01:52:12 +0900

    delete: Friends tab

--- SLPProject/SLPProject/Presentation/View/Main/MainTab/MainTabViewController.swift
+++ SLPProject/SLPProject/Presentation/View/Main/MainTab/MainTabViewController.swift
@@ -19,19 +19,16 @@ final class MainTabViewController: UITabBarController, UITabBarControllerDelegat
         
         let vc1 = HomeTabViewController()
         let vc2 = SeSACShopViewController()
-        let vc3 = SearchStudyViewController()
-        let vc4 = MyInfoViewController()
+        let vc3 = MyInfoViewController()
         
         let firstTabBarItem = UITabBarItem(title: "홈", image: SLPAssets.CustomImage.homeIcon.image, tag: 0)
         let secondTabBarItem = UITabBarItem(title: "새싹샵", image: SLPAssets.CustomImage.shopIcon.image, tag: 1)
-        let thirdTabBarItem = UITabBarItem(title: "새싹친구", image: SLPAssets.CustomImage.friendIcon.image, tag: 2)
         let fourthTabBarItem = UITabBarItem(title: "내정보", image: SLPAssets.CustomImage.homeIcon.image, tag: 3)
         
         vc1.tabBarItem = firstTabBarItem
         vc2.tabBarItem = secondTabBarItem
-        vc3.tabBarItem = thirdTabBarItem
-        vc4.tabBarItem = fourthTabBarItem
+        vc3.tabBarItem = fourthTabBarItem
         
-        viewControllers = [vc1, vc2, vc3, vc4]
+        viewControllers = [vc1, vc2, vc3]
     }
 }
